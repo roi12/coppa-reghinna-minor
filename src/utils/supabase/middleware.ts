c0cc55cca4fbbdca1db1b84fcaa7d6564d48e7e2
Luca <luca.roggi12@outlook.it>
2026-06-12 01:15:14 +0200

    Add Supabase helper setup

---
+++ src/utils/supabase/middleware.ts
@@ -0,0 +1,49 @@
+import { createServerClient } from "@supabase/ssr";
+import { type NextRequest, NextResponse } from "next/server";
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY;
+
+export function createClient(request: NextRequest) {
+  let response = NextResponse.next({
+    request: {
+      headers: request.headers,
+    },
+  });
+
+  const supabase = createServerClient(supabaseUrl!, supabaseKey!, {
+    cookies: {
+      getAll() {
+        return request.cookies.getAll();
+      },
+      setAll(cookiesToSet) {
+        cookiesToSet.forEach(({ name, value }) => {
+          request.cookies.set(name, value);
+        });
+
+        response = NextResponse.next({
+          request,
+        });
+
+        cookiesToSet.forEach(({ name, value, options }) => {
+          response.cookies.set(name, value, options);
+        });
+      },
+    },
+  });
+
+  return {
+    supabase,
+    get response() {
+      return response;
+    },
+  };
+}
+
+export async function updateSession(request: NextRequest) {
+  const middlewareClient = createClient(request);
+
+  await middlewareClient.supabase.auth.getUser();
+
+  return middlewareClient.response;
+}
